3020db3464ecf582b07347981b0b4cf6affd2242
Sonarie <sonarie1980@gmail.com>
2022-01-29 14:10:59 -0600

    color changing boxes complete

--- Assets/Untitled-1.js
+++ Assets/Untitled-1.js
@@ -1,99 +1,124 @@
-//var pastDue = document.getElementsByClassName(".past");
-//var present = document.getElementsByClassName(".present");
-//var future = document.getElementsByClassName(".future");
 var nineEl = document.getElementById("userInput9");
+var tenEl = document.getElementById("userInput10");
+var elevenEl = document.getElementById("userInput11");
+var twelveEl = document.getElementById("userInput12");
+var thirteenEl = document.getElementById("userInput13");
+var fourteenEl = document.getElementById("userInput14");
+var fifteenEl = document.getElementById("userInput15");
+var sixteenEl = document.getElementById("userInput16");
+var seventeenEl = document.getElementById("userInput17");
 
 const d = new Date();
 d.setHours(15);
 document.getElementById("currentDay").innerHTML = d;
 
-let currentTime = moment();
-let dayTime1 = moment("9:00", "hh:mm A");
+let currentTime = moment().hour();
+console.log(currentTime);
 
-if (currentTime.isAfter(dayTime1, "8:59 A")) {
+let dayTime1 = 9;
+
+if (currentTime > dayTime1) {
   nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "10:00 A")) {
+} else if (currentTime < dayTime1) {
   nineEl.classList.add("future");
-} else if (currentTime.isBetween("9:00 A", "9:59 A")) {
-  nineEl.classList.add(".present");
+} else if (currentTime === dayTime1) {
+  nineEl.classList.add("present");
 }
 
-let dayTime2 = moment("10:00", "hh:mm A");
+let dayTime2 = 10;
 
-if (currentTime.isAfter(dayTime1, "9:59 A")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "11:00 A")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("10:00 A", "10:59 A")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime2) {
+  tenEl.classList.add("past");
+} else if (currentTime < dayTime2) {
+  tenEl.classList.add("future");
+} else if (currentTime === dayTime2) {
+  tenEl.classList.add("present");
 }
 
-let dayTime3 = moment("11:00", "hh:mm A");
+let dayTime3 = 11;
 
-if (currentTime.isAfter(dayTime1, "10:59 A")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "12:00 A")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("11:00 A", "11:59 A")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime3) {
+  elevenEl.classList.add("past");
+} else if (currentTime < dayTime3) {
+  elevenEl.classList.add("future");
+} else if (currentTime === dayTime3) {
+  elevenEl.classList.add("present");
 }
 
-let dayTime3 = moment("12:00", "hh:mm P");
+let dayTime4 = 12;
 
-if (currentTime.isAfter(dayTime1, "11:59 A")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "01:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("12:00 P", "12:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime4) {
+  twelveEl.classList.add("past");
+} else if (currentTime < dayTime4) {
+  twelveEl.classList.add("future");
+} else if (currentTime === dayTime4) {
+  twelveEl.classList.add("present");
 }
 
-let dayTime3 = moment("01:00", "hh:mm P");
+let dayTime5 = 13;
 
-if (currentTime.isAfter(dayTime1, "12:59 P")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "02:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("01:00 P", "01:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime5) {
+  thirteenEl.classList.add("past");
+} else if (currentTime < dayTime5) {
+  thirteenEl.classList.add("future");
+} else if (currentTime === dayTime5) {
+  thirteenEl.classList.add("present");
 }
 
-let dayTime3 = moment("02:00", "hh:mm P");
+let dayTime6 = 14;
 
-if (currentTime.isAfter(dayTime1, "01:59 P")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "03:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("02:00 P", "02:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime6) {
+  fourteenEl.classList.add("past");
+} else if (currentTime < dayTime6) {
+  fourteenEl.classList.add("future");
+} else if (currentTime === dayTime6) {
+  fourteenEl.classList.add("present");
 }
 
-let dayTime3 = moment("03:00", "hh:mm P");
+let dayTime7 = 15;
 
-if (currentTime.isAfter(dayTime1, "02:59 P")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "04:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("03:00 P", "03:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime7) {
+  fifteenEl.classList.add("past");
+} else if (currentTime < dayTime7) {
+  fifteenEl.classList.add("future");
+} else if (currentTime === dayTime7) {
+  fifteenEl.classList.add("present");
 }
 
-let dayTime3 = moment("04:00", "hh:mm P");
+let dayTime8 = 16;
 
-if (currentTime.isAfter(dayTime1, "03:59 P")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "05:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("04:00 P", "04:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime8) {
+  sixteenEl.classList.add("past");
+} else if (currentTime < dayTime8) {
+  sixteenEl.classList.add("future");
+} else if (currentTime === dayTime8) {
+  sixteenEl.classList.add("present");
 }
 
-let dayTime3 = moment("05:00", "hh:mm P");
+let dayTime9 = 17;
 
-if (currentTime.isAfter(dayTime1, "04:59 P")) {
-  nineEl.classList.add("past");
-} else if (currentTime.isBefore(dayTime1, "06:00 P")) {
-  nineEl.classList.add("future");
-} else if (currentTime.isBetween("05:00 P", "05:59 P")) {
-  nineEl.classList.add(".present");
+if (currentTime > dayTime9) {
+  seventeenEl.classList.add("past");
+} else if (currentTime < dayTime9) {
+  seventeenEl.classList.add("future");
+} else if (currentTime === dayTime9) {
+  seventeenEl.classList.add("present");
 }
+
+//when save button is clicked, save contents from box to local storage
+//when #save9 icon is clicked, save contents from #userInput9 to local storage
+
+// document.getElementById("save9").addEventListener("click", setValue);
+
+// function setValue() {
+//   console.log("clicked");
+//   localStorage.setItem("nine", nineEl.textContent);
+//   console.log(nineEl.textContent);
+// }
+
+// var test = localStorage.getItem("nine");
+// console.log("worked!" + test);
+
+// nineEl.textContent = test;
+
+//getItem takes from local storage and keeps displying in element
